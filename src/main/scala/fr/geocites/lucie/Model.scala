85c036b9c3104b0060b7dea75cf38fb06849c3ac
Romain Reuillon <romain.reuillon@iscpif.fr>
2016-03-22 11:40:25 +0100

    Intergrate moran.

--- src/main/scala/fr/geocites/lucie/Model.scala
+++ src/main/scala/fr/geocites/lucie/Model.scala
@@ -445,31 +445,90 @@ object Logger {
   type Logger = Event => Unit
 }
 
-/*
+
 object Analyse {
-  def moran[T](quantity: Vector[Vector[Double]], neighbors: Matrix[T] ⇒ Iterator[(T, T, Double)]): Double = {
-    val totalQuantity = quantity.flatten.sum
-    val averageQuantity = totalQuantity / quantity.flatten.size
 
-    def numerator =
-      neighbors(state).map {
-        case (cellI, cellJ, weight) ⇒
-          val term1 = if (quantity(cellI) == 0) 0.0 else (quantity(cellI) - averageQuantity.toDouble)
-          val term2 = if (quantity(cellJ) == 0) 0.0 else (quantity(cellJ) - averageQuantity.toDouble)
-          weight * term1 * term2
-      }.sum
+  import org.apache.commons.math3.util.MathArrays
 
-    def denominator =
-      flatCells.map {
-        cell ⇒
-          if (quantity(cell) <= 0) 0
-          else math.pow(quantity(cell) - averageQuantity.toDouble, 2)
-      }.sum
-
-    val totalWeight = neighbors(state).map { case (_, _, weight) ⇒ weight }.sum
-
-    if (denominator.toDouble <= 0) 0
-    else (state.numberOfCells.toDouble / totalWeight.toDouble) * (numerator / denominator)
+  /**
+    * Moran index using fast convolution.
+    *
+    * @param matrix
+    * @return
+    */
+  def moran(matrix: Vector[Vector[Double]]): Double = {
+    val n = matrix.length
+    val flatConf = matrix.flatten
+    val popMean = flatConf.sum / flatConf.length
+    val centeredConf = matrix.map { r => r.map { d => d - popMean }.toArray }.toArray
+    val variance = MathArrays.ebeMultiply(centeredConf.flatten.toArray, centeredConf.flatten.toArray).sum
+    val weights = spatialWeights(2 * n - 1)
+    val totWeight = Convolution.convolution2D(Array.fill(n, n) { 1.0 }, weights).flatten.sum
+    flatConf.length / (totWeight * variance) * MathArrays.ebeMultiply(centeredConf.flatten.toArray, Convolution.convolution2D(centeredConf, weights).flatten).sum
   }
-}*/
+
+  def spatialWeights(n: Int): Array[Array[Double]] = {
+    Array.tabulate(n, n) { (i, j) => if (i == n / 2 && j == n / 2) 0.0 else 1 / Math.sqrt((i - n / 2) * (i - n / 2) + (j - n / 2) * (j - n / 2)) }
+  }
+
+}
+
+
+object Convolution {
+
+
+  import org.apache.commons.math3.complex.Complex
+  import org.apache.commons.math3.transform.{ TransformType, DftNormalization, FastFourierTransformer }
+  import org.apache.commons.math3.util.MathArrays
+  import math._
+
+  /**
+    * Generic convol for double Arrays (in O(nlog(n)), using FFT)
+    *
+    * @param x
+    * @param k centered kernel
+    * @return y = x*k with y_i = \sum_{j=1}{|K|}{x_{i-j-|K|/2}*k_j}
+    */
+  def convolution(x: Array[Double], k: Array[Double]): Array[Double] = {
+    val xl = pow(2.0, ceil(log(x.length) / log(2.0)) + 1)
+    val xp = x.padTo(x.length + (xl.toInt - x.length) / 2, 0.0).reverse.padTo(xl.toInt, 0.0).reverse
+    val kp = k.padTo(k.length + (xl.toInt - k.length) / 2, 0.0).reverse.padTo(xl.toInt, 0.0).reverse
+    val tr = new FastFourierTransformer(DftNormalization.STANDARD)
+    val ftx = tr.transform(xp, TransformType.FORWARD)
+    val ftk = tr.transform(kp, TransformType.FORWARD)
+    val real = MathArrays.ebeSubtract(MathArrays.ebeMultiply(ftx.map { z => z.getReal }, ftk.map { z => z.getReal }), MathArrays.ebeMultiply(ftx.map { z => z.getImaginary }, ftk.map { z => z.getImaginary }))
+    val im = MathArrays.ebeAdd(MathArrays.ebeMultiply(ftx.map { z => z.getReal }, ftk.map { z => z.getImaginary }), MathArrays.ebeMultiply(ftx.map { z => z.getImaginary }, ftk.map { z => z.getReal }))
+    val trinv = tr.transform(Array.tabulate(real.length) { i => new Complex(real(i), im(i)) }, TransformType.INVERSE).map { z => z.getReal }
+    trinv.splitAt(trinv.length - x.length / 2)._2 ++ trinv.splitAt(x.length - x.length / 2)._1
+  }
+
+  /**
+    * Square convol (for tests)
+    *
+    * @param x
+    * @param k
+    * @return
+    */
+  def directConvol(x: Array[Double], k: Array[Double]): Array[Double] = {
+    val kl = k.length
+    val xpadded = x.padTo(x.length + kl, 0.0).reverse.padTo(x.length + 2 * kl, 0.0).reverse
+    Array.tabulate(x.length + k.length) { i => MathArrays.ebeMultiply(k.reverse, xpadded.splitAt(i + 1)._2.splitAt(k.length)._1).sum }
+  }
+
+  /**
+    *  2D convolution
+    *  Using bijection [|1,N|]2 ~ [|1,N|] by flattening, after having good paddling
+    *
+    * @param x
+    * @param k
+    */
+  def convolution2D(x: Array[Array[Double]], k: Array[Array[Double]]): Array[Array[Double]] = {
+    val xpad = x.map { row => row.padTo(2 * (row.length / 2) + 1 + k(0).length, 0.0).reverse.padTo(2 * (row.length / 2) + 1 + 2 * k(0).length, 0.0).reverse }.padTo(2 * (x.length / 2) + 1 + k.length, Array.fill(2 * (x(0).length / 2) + 1 + 2 * k(0).length) { 0.0 }).reverse.padTo(2 * (x.length / 2) + 1 + 2 * k.length, Array.fill(2 * (x(0).length / 2) + 1 + 2 * k(0).length) { 0.0 })
+    val xpos = Array.fill(x.length, x(0).length) { 1.0 }.map { row => row.padTo(2 * (row.length / 2) + 1 + k(0).length, 0.0).reverse.padTo(2 * (row.length / 2) + 1 + 2 * k(0).length, 0.0).reverse }.padTo(2 * (x.length / 2) + 1 + k.length, Array.fill(2 * (x(0).length / 2) + 1 + 2 * k(0).length) { 0.0 }).reverse.padTo(2 * (x.length / 2) + 1 + 2 * k.length, Array.fill(2 * (x(0).length / 2) + 1 + 2 * k(0).length) { 0.0 }).flatten
+    val kpad = k.map { row => row.padTo(row.length + (xpad(0).length - row.length) / 2, 0.0).reverse.padTo(xpad(0).length, 0.0).reverse }.padTo(k.length + (xpad.length - k.length) / 2, Array.fill(xpad(0).length) { 0.0 }).reverse.padTo(xpad.length, Array.fill(xpad(0).length) { 0.0 })
+    val flatconv = convolution(xpad.flatten, kpad.flatten)
+    flatconv.zipWithIndex.filter { case (_, j) => xpos(j) > 0 }.map { case (d, _) => d }.sliding(x(0).length, x.length).toArray.reverse
+  }
+
+}
 
